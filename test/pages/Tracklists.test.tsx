c353cf0285c5c9a675221f63472e19f708d3a389
Tom Bell <tomb@tomb.io>
2019-02-24 16:29:16 +0000

    Add tracklists page component tests

---
+++ test/pages/Tracklists.test.tsx
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import { render } from 'preact-render-spy';
+
+import test from 'ava';
+
+import Tracklists from '../../src/pages/Tracklists';
+
+test('renders tracklists component', async (t) => {
+  const ctx = render(<Tracklists />);
+  t.is(ctx.find('h2').exists(), true, 'has top-level header');
+  t.is(ctx.find('h2').text(), 'Tracklists', 'has top-level header text');
+});

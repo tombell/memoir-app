351d47e736c86120c479c764503d442d0c66278d
Tom Bell <tomb@tomb.io>
2019-02-24 16:21:59 +0000

    Add app component tests

---
+++ test/components/App.test.tsx
@@ -0,0 +1,13 @@
+import { h } from 'preact';
+import { render } from 'preact-render-spy';
+
+import test from 'ava';
+
+import App from '../../src/components/App';
+
+test('renders app component', async (t) => {
+  const ctx = render(<App />);
+  t.is(ctx.find('div').exists(), true, 'has parent container');
+  t.is(ctx.find('h1').exists(), true, 'has top-level header');
+  t.is(ctx.find('h1').text(), 'Memoir', 'has top-level header text');
+});

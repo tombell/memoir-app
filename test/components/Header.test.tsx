82dd3049565dba2565859db0289abce0c1b364f3
Tom Bell <tomb@tomb.io>
2019-02-24 16:14:46 +0000

    Add header component tests

---
+++ test/components/Header.test.tsx
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import { shallow } from 'preact-render-spy';
+
+import test from 'ava';
+
+import Header from '../../src/components/Header';
+
+test('renders header component', async (t) => {
+  const ctx = shallow(<Header />);
+  t.is(ctx.find('h1').text(), 'Memoir', 'header text is correct');
+});

7dcdf5b8c35d06dbc55d0b9d7dd0f53c96038e4d
Tom Bell <tomb@tomb.io>
2019-02-24 17:31:24 +0000

    Add initial styles for pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,21 @@
 import { h } from 'preact';
 import { Router } from 'preact-router';
 
+import { sheet } from '../nano';
+
 import Header from './Header';
 
 import Tracklists from '../pages/Tracklists';
 
+const styles = sheet!({
+  page: {
+    margin: '0 auto',
+    width: '620px',
+  },
+});
+
 export default () => (
-  <div>
+  <div class={styles.page}>
     <Header />
     <Router>
       <Tracklists path="/" />

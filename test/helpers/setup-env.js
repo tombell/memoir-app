626a52d04ee2e30c0e16247d0f7f85e1d06ccd9a
Tom Bell <tomb@tomb.io>
2019-02-24 17:19:37 +0000

    Update setup-env.js to stub document.getElementsByTagName

--- test/helpers/setup-env.js
+++ test/helpers/setup-env.js
@@ -2,3 +2,6 @@ import 'undom/register';
 import { config } from 'preact-render-spy';
 
 config.createFragment = () => document.createElement('body');
+
+// set document.getElementsByTagName to satisfy nano-css
+document.getElementsByTagName = () => [];

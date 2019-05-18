9a349ad4a6466201ac3e6f78a5adc39c0c641aad
Tom Bell <tomb@tomb.io>
2019-05-18 23:20:09 +0100

    Render some tracklists

--- src/pages/Tracklists.tsx
+++ src/pages/Tracklists.tsx
@@ -1,5 +1,55 @@
-import { h, FunctionalComponent } from 'preact';
+import { h, Component } from 'preact';
 
-const Tracklists: FunctionalComponent = () => <h2>Tracklists</h2>;
+import { Tracklist, fetchTracklists } from '../services/memoir';
 
-export default Tracklists;
+interface State {
+  isLoading: boolean;
+  tracklists: Tracklist[] | null;
+}
+
+export default class Tracklists extends Component<any, State> {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      tracklists: null,
+    };
+  }
+
+  async componentWillMount() {
+    this.setState({ isLoading: true });
+    const tracklists = await fetchTracklists();
+    this.setState({ isLoading: false, tracklists });
+  }
+
+  renderTracklists() {
+    const { tracklists } = this.state;
+
+    if (!tracklists) {
+      return null;
+    }
+
+    return tracklists.map(tracklist => <p>{tracklist.name}</p>);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) {
+      return (
+        <div>
+          <h2>Tracklists</h2>
+          <p>Loading...</p>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h2>Tracklists</h2>
+        {this.renderTracklists()}
+      </div>
+    );
+  }
+}

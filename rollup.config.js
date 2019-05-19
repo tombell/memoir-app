f7163e3aaff898d9da83aa4798aaa08da083dc21
Tom Bell <tomb@tomb.io>
2019-05-19 11:37:39 +0100

    Update to use rollup-plugin-serve instead

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import serve from 'rollup-plugin-serve';
 import { uglify } from 'rollup-plugin-uglify';
 
 const production = process.env.BUILD === 'production';
@@ -27,6 +28,7 @@ export default {
     resolve(),
     replace(config),
     typescript(),
+    !production && serve('public'),
     production && uglify(),
   ],
 };

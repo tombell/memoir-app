dcce0c10ea304e1cbf08f05311e14ad1d54df454
Tom Bell <tomb@tomb.io>
2019-05-19 17:50:17 +0100

    Fix import casing

--- test/components/Tag.test.tsx
+++ test/components/Tag.test.tsx
@@ -2,7 +2,7 @@ import { h } from 'preact';
 import { shallow } from 'preact-render-spy';
 import test from 'ava';
 
-import Tag from '../../src/components/tag';
+import Tag from '../../src/components/Tag';
 
 test('renders tag component', async t => {
   const ctx = shallow(<Tag label="Test" />);

3adbac6ff61967b29cf469d31e288507fd8e174e
Tom Bell <tomb@tomb.io>
2019-05-19 17:07:05 +0100

    Add TracklistItem component

---
+++ src/components/TracklistItem.tsx
@@ -0,0 +1,18 @@
+import { h } from 'preact';
+import { Link } from 'preact-router';
+
+import { Tracklist } from '../services/memoir/types';
+
+interface Props {
+  tracklist: Tracklist;
+}
+
+export default (props: Props) => {
+  const { tracklist } = props;
+
+  return (
+    <p>
+      <Link href={`/${tracklist.id}`}>{tracklist.name}</Link>
+    </p>
+  );
+}

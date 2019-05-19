1f20463b0d5aa4bd88700e0e623e641911dda2d7
Tom Bell <tomb@tomb.io>
2019-05-19 16:41:40 +0100

    Add global addon to nano service

--- src/services/nano.ts
+++ src/services/nano.ts
@@ -1,12 +1,14 @@
 import { create } from 'nano-css';
+import { addon as addonGlobal } from 'nano-css/addon/global';
 import { addon as addonRule } from 'nano-css/addon/rule';
 import { addon as addonSheet } from 'nano-css/addon/sheet';
 
 const nano = create();
 
+addonGlobal(nano);
 addonRule(nano);
 addonSheet(nano);
 
-const { rule, sheet } = nano;
+const { global, rule, sheet } = nano;
 
-export { rule, sheet, nano };
+export { global, rule, sheet, nano };

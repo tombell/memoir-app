6031c8452f29a6b06636133590cdac8080e302ee
Tom Bell <tomb@tomb.io>
2019-05-19 20:38:13 +0100

    Remove useless quotes

--- src/components/Tag.styles.ts
+++ src/components/Tag.styles.ts
@@ -7,7 +7,7 @@ export default sheet!({
     color: '#e8dcd8',
     'font-size': '11px',
     'font-weight': 'bold',
-    'margin': '0 5px 0 0',
+    margin: '0 5px 0 0',
     padding: '3px 5px',
   },
 });

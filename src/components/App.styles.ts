f43f897bf4c7264f3c38625a401487e324179b20
Tom Bell <tomb@tomb.io>
2019-05-19 16:42:13 +0100

    Update app component styles

--- src/components/App.styles.ts
+++ src/components/App.styles.ts
@@ -1,8 +1,16 @@
-import { sheet } from '../services/nano';
+import { global, sheet } from '../services/nano';
+
+global!({
+  body: {
+    background: '#2c2a31',
+    color: '#e8dcd8',
+    'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  },
+});
 
 export default sheet!({
   page: {
     margin: '0 auto',
-    width: '620px',
+    width: '690px',
   },
 });

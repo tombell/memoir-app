bec34a9a09944d8da3d22060ab5a745777fda7af
Tom Bell <tomb@tomb.io>
2019-05-19 14:08:13 +0100

    Update component tests to pass in stub fetch functions

---
+++ test/pages/Tracklist.test.tsx
@@ -0,0 +1,22 @@
+import { h } from 'preact';
+import { render } from 'preact-render-spy';
+import test from 'ava';
+
+import TracklistPage from '../../src/pages/Tracklist';
+
+import { Tracklist } from '../../src/services/memoir';
+
+const fetchTracklistStub = (id: string): Promise<Tracklist | null> => (
+  new Promise(resolve => resolve({
+    id,
+    name: 'Testing',
+    date: '2019-05-19T00:00:00Z',
+  }))
+);
+
+test('renders tracklist component', async t => {
+  const ctx = render(<TracklistPage id="1" fetchTracklist={fetchTracklistStub} />);
+
+  t.is(ctx.find('h2').exists(), true, 'has top-level header');
+  t.is(ctx.find('h2').text(), 'Tracklist: 1', 'has top-level header text');
+});
--- test/pages/Tracklists.test.tsx
+++ test/pages/Tracklists.test.tsx
@@ -1,12 +1,15 @@
 import { h } from 'preact';
 import { render } from 'preact-render-spy';
-
 import test from 'ava';
 
 import TracklistsPage from '../../src/pages/Tracklists';
 
+import { Tracklist } from '../../src/services/memoir';
+
+const fetchTracklistsStub = (): Promise<Tracklist[] | null> => new Promise(resolve => resolve([]));
+
 test('renders tracklists component', async t => {
-  const ctx = render(<TracklistsPage />);
+  const ctx = render(<TracklistsPage fetchTracklists={fetchTracklistsStub} />);
   t.is(ctx.find('h2').exists(), true, 'has top-level header');
   t.is(ctx.find('h2').text(), 'Tracklists', 'has top-level header text');
 });

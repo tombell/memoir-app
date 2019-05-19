dc13695fb8984408a68da4ece1f34373423d30c7
Tom Bell <tomb@tomb.io>
2019-05-19 20:04:33 +0100

    Update tag component styles

--- src/components/Tag.styles.ts
+++ src/components/Tag.styles.ts
@@ -2,12 +2,12 @@ import { sheet } from '../services/nano';
 
 export default sheet!({
   tag: {
-    background: '#927e77',
+    background: '#424e2a',
     'border-radius': '3px',
     color: '#e8dcd8',
     'font-size': '11px',
     'font-weight': 'bold',
+    'margin': '0 5px 0 0',
     padding: '3px 5px',
-    'margin-right': '5px',
   },
 });

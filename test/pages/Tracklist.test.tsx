d2156625619387341e7c9d76477f9a8f336f61e6
Tom Bell <tomb@tomb.io>
2019-05-20 23:12:33 +0100

    Clean up page acceptance tests

--- test/pages/Tracklist.test.tsx
+++ test/pages/Tracklist.test.tsx
@@ -6,21 +6,19 @@ import TracklistPage from '../../src/pages/Tracklist';
 
 import { FetchTracklist } from '../../src/services/memoir/types';
 
-const fetchTracklistStub: FetchTracklist = (id) => (
-  new Promise(resolve => resolve({
-    id,
-    name: 'Testing',
-    date: '2019-05-19T00:00:00Z',
-    trackCount: 19,
-  }))
-);
-
 test('renders tracklist component', async t => {
-  const ctx = render(<TracklistPage id="1" fetchTracklist={fetchTracklistStub} />);
+  const fetchStub: FetchTracklist = (id) => (
+    new Promise(resolve => resolve({
+      id,
+      name: 'Testing',
+      date: '2019-05-19T00:00:00Z',
+      trackCount: 19,
+    }))
+  );
 
-  t.truthy(ctx, 'TODO');
+  const ctx = render(<TracklistPage id="1" fetchTracklist={fetchStub} />);
 
   // TODO: implement proper tests
-  // t.is(ctx.find('h2').exists(), true, 'has top-level header');
-  // t.is(ctx.find('h2').text(), 'Tracklist: 1', 'has top-level header text');
+
+  t.truthy(ctx, 'TODO');
 });
--- test/pages/Tracklists.test.tsx
+++ test/pages/Tracklists.test.tsx
@@ -6,14 +6,12 @@ import TracklistsPage from '../../src/pages/Tracklists';
 
 import { FetchTracklists } from '../../src/services/memoir/types';
 
-const fetchTracklistsStub: FetchTracklists = () => new Promise(resolve => resolve([]));
+test('renders empty tracklists component', async t => {
+  const fetchStub: FetchTracklists = () => new Promise(resolve => resolve([]));
 
-test('renders tracklists component', async t => {
-  const ctx = render(<TracklistsPage fetchTracklists={fetchTracklistsStub} />);
-
-  t.truthy(ctx, 'TODO');
+  const ctx = render(<TracklistsPage fetchTracklists={fetchStub} />);
 
   // TODO: implement proper tests
-  // t.is(ctx.find('h2').exists(), true, 'has top-level header');
-  // t.is(ctx.find('h2').text(), 'Tracklists', 'has top-level header text');
+
+  t.truthy(ctx, 'TODO');
 });

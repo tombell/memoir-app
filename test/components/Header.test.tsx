54e88f40a279703ffe3789309102149e0c5cf00f
Tom Bell <tomb@tomb.io>
2019-05-20 02:49:15 +0100

    Add test for header href attr

--- test/components/Header.test.tsx
+++ test/components/Header.test.tsx
@@ -1,10 +1,11 @@
 import { h } from 'preact';
-import { shallow } from 'preact-render-spy';
+import { render } from 'preact-render-spy';
 import test from 'ava';
 
 import Header from '../../src/components/Header';
 
 test('renders header component', async t => {
-  const ctx = shallow(<Header />);
-  t.is(ctx.find('h1').text(), 'Memoir', 'header text is correct');
+  const ctx = render(<Header />);
+  t.is(ctx.find('h1').text(), 'Memoir', 'header text');
+  t.is(ctx.find<any, {}>('a').attr('href'), '/', 'header link href attr');
 });

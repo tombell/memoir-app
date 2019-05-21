f681c2183822a0078cf8bd13c96537828cd4b675
Tom Bell <tomb@tomb.io>
2019-05-21 21:13:54 +0100

    Nicer wrapping on tags

--- src/components/Tag.styles.ts
+++ src/components/Tag.styles.ts
@@ -6,9 +6,10 @@ export default sheet!({
     background: colors.secondary,
     borderRadius: '3px',
     color: '#fff',
+    display: 'inline-block',
     fontSize: '11px',
     fontWeight: 'bold',
-    margin: '0 5px 0 0',
+    margin: '0 5px 5px 0',
     padding: '3px 5px',
   },
 }, 'tag');

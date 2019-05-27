a5436eea6375dd461df5efe7445fdfc1a0d54634
Tom Bell <tomb@tomb.io>
2019-05-27 17:05:32 +0100

    Avoid the redirect when clicking the header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { h } from 'preact';
 
 export default () => (
   <header class="header">
-    <a class="header-link" href="/">
+    <a class="header-link" href="/tracklists/1">
       <img class="header-logo" src="/images/logo.svg" alt="Memoir" />
     </a>
   </header>

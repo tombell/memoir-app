a5a19f3813d54d6286615b7f823c08758440829b
Tom Bell <tomb@tomb.io>
2019-05-28 12:59:28 +0100

    Add not found page component

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import { h, FunctionalComponent } from 'preact';
+
+const NotFoundPage: FunctionalComponent = () => {
+  return (
+    <div class="not-found">
+      <h2 class="not-found-header">Page Not Found</h2>
+    </div>
+  );
+};
+
+export default NotFoundPage;

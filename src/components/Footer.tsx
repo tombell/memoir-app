9ae62b9f0e867da42c260845fb5e66c8146b8a38
Tom Bell <tomb@tomb.io>
2019-05-28 12:55:00 +0100

    Update formatting and links in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,17 +4,33 @@ export default () => {
   return (
     <footer class="footer">
       <div class="footer-left">
-        <a class="footer-link" href="/tracklists/1">Tracklists</a>
-        <a class="footer-link" href="/tracks/mostplayed">Most Played Tracks</a>
-        <a class="footer-link" href="/about">About</a>
+        <a class="footer-link" href="/tracklists/1">
+          Tracklists
+        </a>
+        <a class="footer-link" href="/tracks/mostplayed">
+          Most Played Tracks
+        </a>
+        <a class="footer-link" href="/about">
+          About
+        </a>
       </div>
       <div class="footer-right">
-        <a class="footer-link" href="https://mixcloud.com/iamdjriff">Mixcloud</a>
-        <a class="footer-link" href="https://twitch.tv/iamdjriff">Twitch</a>
-        <a class="footer-link" href="https://facebook/iamdjriff">Facebook</a>
-        <a class="footer-link" href="https://twitter.com/iamdjriff">Twitter</a>
-        <a class="footer-link" href="https://instagram.com/iamdjriff">Instagram</a>
+        <a class="footer-link" href="https://mixcloud.com/iamdjriff">
+          Mixcloud
+        </a>
+        <a class="footer-link" href="https://twitch.tv/iamdjriff">
+          Twitch
+        </a>
+        <a class="footer-link" href="https://facebook/iamdjriff">
+          Facebook
+        </a>
+        <a class="footer-link" href="https://twitter.com/iamdjriff">
+          Twitter
+        </a>
+        <a class="footer-link" href="https://instagram.com/iamdjriff">
+          Instagram
+        </a>
       </div>
     </footer>
-  )
+  );
 };

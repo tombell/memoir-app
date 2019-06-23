9b7cfc6f3729e387e5728a5ada45f57b2673ed60
Tom Bell <tomb@tomb.io>
2019-06-23 15:02:46 +0100

    Update tracklist item date into a tag

--- src/components/TracklistItem.tsx
+++ src/components/TracklistItem.tsx
@@ -14,15 +14,12 @@ export default ({ tracklist }: Props) => {
   return (
     <a class="tracklist-item-link" href={`/tracklist/${tracklist.id}`}>
       <div class="tracklist-item">
-        <div class="tracklist-item-date">
-          <span>{formatDate(tracklist.date)}</span>
-        </div>
-
         <div class="tracklist-item-details">
           <h3 class="tracklist-item-name">{tracklist.name}</h3>
 
-          <div class="tracklist-item-tracks">
+          <div class="tracklist-item-tags">
             <Tag text={`${tracklist.trackCount} Tracks`} color="green" />
+            <Tag text={formatDate(tracklist.date)} color="light-blue" />
           </div>
         </div>
       </div>

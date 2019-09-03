5933513e7275976c5fd669c5b8f2dcac3bd57903
Tom Bell <tomb@tomb.io>
2019-09-03 21:02:24 +0100

    Add content to the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,14 @@ const AboutPage: FunctionalComponent = () => {
   return (
     <div class="about">
       <h2 class="about-header">About Memoir</h2>
+      <p>
+        Memoir was built to be a project to experiment with a Go API and Preact
+        front-end.
+      </p>
+      <p>
+        Also with Mixcloud not displaying track lists to listeners, I wanted a
+        great way of sharing track lists for my mixes.
+      </p>
       <Footer />
     </div>
   );

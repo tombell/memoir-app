86ac84a2a68c9f7b9a2f3b604d2d0bbe396e8b51
Tom Bell <tomb@tomb.io>
2019-11-30 16:12:24 +0000

    Finish up

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,40 +7,21 @@ interface Props {
 }
 
 interface State {
-  isLoading: boolean;
+  showResults: boolean;
   tracks: Track[] | null;
-  hasMore: boolean;
 }
 
 export default class Search extends Component<Props, State> {
-  private loadingTimer?: NodeJS.Timeout;
-
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      isLoading: false,
+      showResults: false,
       tracks: null,
-      hasMore: false,
     };
   }
 
-  showLoadingIndicator = () => {
-    this.loadingTimer = setTimeout(
-      () => this.setState({ isLoading: true }),
-      1000
-    );
-  };
-
-  hideLoadingIndicator = () => {
-    if (this.loadingTimer) {
-      clearTimeout(this.loadingTimer);
-    }
-
-    this.setState({ isLoading: false });
-  };
-
-  onInput = (event: any) => {
+  onSearchInput = (event: any) => {
     const { value } = event.target;
 
     if (value.length < 3) {
@@ -50,19 +31,49 @@ export default class Search extends Component<Props, State> {
     this.searchTracks(value);
   };
 
-  async searchTracks(query: string) {
-    this.showLoadingIndicator();
+  showResults = () => {
+    this.setState({ showResults: true });
+  };
 
+  hideResults = () => {
+    this.setState({ showResults: false });
+  };
+
+  async searchTracks(query: string) {
     const { searchTracks } = this.props;
     const paged = await searchTracks(query);
 
-    this.hideLoadingIndicator();
-
     if (paged) {
-      const { tracks, hasMore } = paged;
+      const { tracks } = paged;
 
-      this.setState({ tracks, hasMore });
+      this.setState({ tracks, showResults: true });
     }
+  }
+
+  renderSearchResults() {
+    const { tracks, showResults } = this.state;
+
+    if (!tracks || !showResults) {
+      return null;
+    }
+
+    return (
+      <div class="search__results">
+        <ul class="search__results-list">
+          {tracks.map(t => this.renderSearchResult(t))}
+        </ul>
+      </div>
+    );
+  }
+
+  renderSearchResult(track: Track) {
+    return (
+      <li class="search__results-item">
+        <a href={`/track/${track.id}`} onClick={this.hideResults}>
+          {`${track.artist} - ${track.name}`}
+        </a>
+      </li>
+    );
   }
 
   render() {
@@ -72,33 +83,12 @@ export default class Search extends Component<Props, State> {
           <input
             class="search__input"
             placeholder="Search tracks..."
-            onInput={this.onInput}
+            onInput={this.onSearchInput}
+            onFocus={this.showResults}
           />
         </div>
 
         {this.renderSearchResults()}
-      </div>
-    );
-  }
-
-  renderSearchResults() {
-    const { tracks } = this.state;
-
-    if (!tracks) {
-      return null;
-    }
-
-    return (
-      <div class="search__results">
-        <ul class="search__results-list">
-          {tracks.map(track => (
-            <li class="search__results-item">
-              <a href={`/track/${track.id}`}>
-                {track.artist} - {track.name}
-              </a>
-            </li>
-          ))}
-        </ul>
       </div>
     );
   }

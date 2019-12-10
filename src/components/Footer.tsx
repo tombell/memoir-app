0f30de767a2ad5b501002105eca46c1f3b921448
Tom Bell <tomb@tomb.io>
2019-12-10 23:41:01 +0000

    Remove old social accounts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,17 +18,8 @@ export default () => {
         <a class="footer__link" href="https://mixcloud.com/iamdjriff">
           Mixcloud
         </a>
-        <a class="footer__link" href="https://twitch.tv/iamdjriff">
-          Twitch
-        </a>
-        <a class="footer__link" href="https://facebook/iamdjriff">
-          Facebook
-        </a>
         <a class="footer__link" href="https://twitter.com/iamdjriff">
           Twitter
-        </a>
-        <a class="footer__link" href="https://instagram.com/iamdjriff_">
-          Instagram
         </a>
       </div>
     </footer>

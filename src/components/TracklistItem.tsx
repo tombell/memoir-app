cd82256be379d5ff880e8319978b0005f45c0079
Tom Bell <tomb@tomb.io>
2020-05-06 21:46:21 +0100

    Add tracklist artwork to tracklist items

--- src/components/TracklistItem.tsx
+++ src/components/TracklistItem.tsx
@@ -15,11 +15,21 @@ export default ({ tracklist }: Props) => {
     <a href={`/tracklist/${tracklist.id}`}>
       <div class="tracklist-item">
         <div class="tracklist-item__details">
-          <h3 class="tracklist-item__name">{tracklist.name}</h3>
+          <div class="tracklist-item__image">
+            <img
+              class="tracklist-item__artwork"
+              alt={`${tracklist.name} Artwork`}
+              src={`${MEMOIR_CDN}/${tracklist.artwork}`}
+            />
+          </div>
 
-          <div class="tracklist-item__tags">
-            <Tag text={`${tracklist.trackCount} Tracks`} color="lilac" />
-            <Tag text={formatDate(tracklist.date)} color="light-blue" />
+          <div class="tracklist-item__meta">
+            <h3 class="tracklist-item__name">{tracklist.name}</h3>
+
+            <div class="tracklist-item__tags">
+              <Tag text={`${tracklist.trackCount} Tracks`} color="lilac" />
+              <Tag text={formatDate(tracklist.date)} color="light-blue" />
+            </div>
           </div>
         </div>
       </div>

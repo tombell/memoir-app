e8d5d0fcadc77015306ae56eed4202d7712b0750
Tom Bell <tomb@tomb.io>
2020-05-06 01:09:48 +0100

    Switch to rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-
 import serve from 'rollup-plugin-serve';
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
 const production = process.env.BUILD === 'production';

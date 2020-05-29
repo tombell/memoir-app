8b408c30b544c1fe0c064c8544319b6310ee7f67
Tom Bell <tomb@tomb.io>
2020-05-29 19:01:52 +0100

    Add rekordbox tracklist parser

---
+++ src/utils/tracklist-parser.ts
@@ -0,0 +1,15 @@
+export default (text: string) => {
+  const tracks: string[][] = [];
+
+  text.split('\n').forEach((line) => {
+    const parts = line.split('\t');
+
+    if (parts[0] === '#' || parts.length <= 1) {
+      return;
+    }
+
+    tracks.push([parts[2], parts[3], parts[6], parts[9], parts[5]]);
+  });
+
+  return tracks;
+};

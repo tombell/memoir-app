43b97f8be2cc4c91ed57032df5e9d2042b101abb
Tom Bell <tomb@tomb.io>
2020-06-10 00:38:54 +0100

    Move tracklist pages into directory

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,15 @@ import { Router } from 'preact-router';
 
 import API from 'memoir-api';
 
-import AddTracklistPage from 'pages/AddTracklist';
-import EditTracklistPage from 'pages/EditTracklist';
+import TracklistsIndex from 'pages/Tracklists/Index';
+import TracklistsShow from 'pages/Tracklists/Show';
+import TracklistsAdd from 'pages/Tracklists/Add';
+import TracklistsEdit from 'pages/Tracklists/Edit';
+
 import MostPlayedTracksPage from 'pages/MostPlayedTracks';
-import NotFoundPage from 'pages/NotFound';
-import TracklistPage from 'pages/Tracklist';
 import TracklistsByTrackPage from 'pages/TracklistsByTrack';
-import TracklistsPage from 'pages/Tracklists';
+
+import NotFoundPage from 'pages/NotFound';
 
 import Header from 'components/Header';
 import Redirect from 'components/Redirect';
@@ -25,14 +27,17 @@ export default () => {
         <Search />
         <Router>
           <Redirect path="/" to="/tracklists/1" />
-          <TracklistsPage path="/tracklists/:page" api={api} />
+
+          <TracklistsIndex path="/tracklists/:page" api={api} />
           {MEMOIR_ADMIN_ENABLED && [
-            <AddTracklistPage path="/tracklists/add" api={api} />,
-            <EditTracklistPage path="/tracklists/edit/:id" api={api} />,
+            <TracklistsAdd path="/tracklists/add" api={api} />,
+            <TracklistsEdit path="/tracklists/edit/:id" api={api} />,
           ]}
-          <TracklistPage path="/tracklist/:id" api={api} />
+          <TracklistsShow path="/tracklist/:id" api={api} />
+
           <TracklistsByTrackPage path="/track/:id/:page?" api={api} />
           <MostPlayedTracksPage path="/tracks/mostplayed" api={api} />
+
           <NotFoundPage path="/404" default />
         </Router>
       </div>

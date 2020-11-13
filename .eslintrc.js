c6999ead81b0ca3748eea88a3e60577b7a63090a
Tom Bell <tomb@tomb.io>
2020-11-13 22:29:25 +0000

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,13 @@ module.exports = {
     },
   },
   rules: {
+    '@typescript-eslint/comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'never',
+      'imports': 'always-multiline',
+      'objects': 'always-multiline',
+    }],
     'react/jsx-props-no-spreading': ['error', { exceptions: ['App'] }],
     'react/no-unknown-property': ['error', { ignore: ['class'] }],
     'react/prop-types': 'off',

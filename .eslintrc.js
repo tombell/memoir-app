22c9a1951807ea8032b2a9536f92119858af15fc
Tom Bell <tomb@tomb.io>
2021-03-21 18:51:45 +0000

    Remove '@typescript-eslint/comma-dangle' from .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,13 +13,6 @@ module.exports = {
     },
   },
   rules: {
-    '@typescript-eslint/comma-dangle': ['error', {
-      'arrays': 'always-multiline',
-      'exports': 'always-multiline',
-      'functions': 'never',
-      'imports': 'always-multiline',
-      'objects': 'always-multiline',
-    }],
     'react/no-unknown-property': ['error', { ignore: ['class'] }],
     'react/prop-types': 'off',
     'react/require-default-props': 'off',

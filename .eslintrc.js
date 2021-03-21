414fb2919107e28ac9514142290e75f0f84995fd
Tom Bell <tomb@tomb.io>
2021-03-21 18:50:46 +0000

    Remove 'react/jsx-props-no-spreading' from .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
       'imports': 'always-multiline',
       'objects': 'always-multiline',
     }],
-    'react/jsx-props-no-spreading': ['error', { exceptions: ['App'] }],
     'react/no-unknown-property': ['error', { ignore: ['class'] }],
     'react/prop-types': 'off',
     'react/require-default-props': 'off',

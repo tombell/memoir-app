d27f8136854323e771169c0a57ab81baa1903ab5
Tom Bell <tomb@tomb.io>
2021-06-21 23:21:04 +0100

    Refactor Genres.tsx to use g-style

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,13 +1,19 @@
 import { h } from "preact";
+import { css } from "g-style";
 
 import Tag from "components/Tag";
+
+const className = css({
+  margin: "0 0.5rem 1rem",
+  textAlign: "center",
+});
 
 interface Props {
   genres: string[];
 }
 
 export default ({ genres }: Props) => (
-  <div class="genres">
+  <div class={className}>
     {genres.sort().map((genre) => (
       <Tag text={genre} color="blue" />
     ))}

5746d7463bac68058ca28b228494b1c24e5dd795
Tom Bell <tomb@tomb.io>
2021-06-21 22:54:23 +0100

    Add Colors enum

---
+++ src/components/atoms/Colors.ts
@@ -0,0 +1,16 @@
+enum Colors {
+  black = "#000000",
+  white = "#e8dcd8",
+  grey = "#cccccc",
+
+  purple = "#b433ff",
+  lilac = "#aa72ff",
+  blue = "#6473ff",
+  green = "#0fb175",
+  lightBlue = "#50b4ff",
+
+  primary = white,
+  secondary = green,
+}
+
+export default Colors;

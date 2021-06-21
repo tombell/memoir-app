4d379d37f0d7761d12926391ae9ea59812d21f50
Tom Bell <tomb@tomb.io>
2021-06-21 23:30:16 +0100

    Refactor TrackItem.tsx to use g-style

--- src/components/TrackItem.tsx
+++ src/components/TrackItem.tsx
@@ -1,20 +1,38 @@
 import { h } from "preact";
+import { css } from "g-style";
 
 import { Track } from "services/memoir";
 
 import Tag from "./Tag";
+
+import Breakpoints from "components/atoms/Breakpoints";
+
+const className = css({
+  flexDirection: "column",
+  alignItems: "center",
+  margin: "0 0.5rem 1rem 0.5rem",
+  [Breakpoints.Desktop]: {
+    margin: "0 0 1.5rem 0",
+  },
+});
+
+const nameClassName = css({
+  margin: "0 0 0.5rem 0",
+  fontWeight: "bold",
+  lineHeight: 1.2,
+});
 
 interface Props {
   track: Track;
 }
 
 export default ({ track }: Props) => (
-  <div class="track-item">
-    <div class="track-item__name">
+  <div class={className}>
+    <div class={nameClassName}>
       <a href={`/track/${track.id}`}>{`${track.artist} - ${track.name}`}</a>
     </div>
 
-    <div class="track-item__tags">
+    <div>
       <Tag text={track.bpm.toFixed(2)} color="purple" />
       <Tag text={track.key} color="lilac" />
       <Tag text={track.genre} color="blue" />

1b5040795c50da2f56014bdde0a70b213272cf27
Tom Bell <tomb@tomb.io>
2021-06-21 23:32:14 +0100

    Refactor Submit.tsx to use g-style

--- src/components/form/Submit.tsx
+++ src/components/form/Submit.tsx
@@ -1,11 +1,23 @@
 import { h } from "preact";
+import { css } from "g-style";
+
+import Colors from "components/atoms/Colors";
+
+const className = css({
+  padding: "0.75rem 1rem",
+  fontWeight: "bold",
+  color: Colors.primary,
+  /* background: darken($background-color, 5%); */
+  border: 0,
+  borderRadius: "0.1875rem",
+});
 
 interface Props {
   onClick: () => void;
 }
 
 export default ({ onClick }: Props) => (
-  <button type="submit" class="submit" onClick={onClick}>
+  <button class={className} type="submit" onClick={onClick}>
     Submit
   </button>
 );

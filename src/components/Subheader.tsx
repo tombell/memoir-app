a45b78aee78d94a30182b38019bd83d4418a33da
Tom Bell <tomb@tomb.io>
2021-06-21 22:57:25 +0100

    Refactor Subheader.tsx to use g-style

--- src/components/Subheader.tsx
+++ src/components/Subheader.tsx
@@ -1,7 +1,14 @@
 import { h } from "preact";
+import { css } from "g-style";
+
+const className = css({
+  marginBottom: "1rem",
+  fontWeight: "bold",
+  textAlign: "center",
+});
 
 interface Props {
   text: string;
 }
 
-export default ({ text }: Props) => <h2 class="subheader">{text}</h2>;
+export default ({ text }: Props) => <h2 class={className}>{text}</h2>;

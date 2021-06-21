ada60f59c048d972a091cf321a33ad19b811f6e0
Tom Bell <tomb@tomb.io>
2021-06-21 22:21:36 +0100

    Refactor Header.tsx to use g-style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,30 @@
 import { h } from "preact";
+import { css } from "g-style";
+
+import Breakpoints from "components/atoms/Breakpoints";
+
+const className = css({
+  width: 60,
+  margin: "1rem auto",
+  [Breakpoints.Desktop]: {
+    width: 120,
+    marginBottom: "4rem",
+  },
+});
+
+const logoClassName = css({
+  width: 60,
+  height: 44,
+  [Breakpoints.Desktop]: {
+    width: 120,
+    height: 88,
+  },
+});
 
 export default () => (
-  <header class="header">
-    <a class="header__link" href="/tracklists/1">
-      <img class="header__logo" src="/images/logo.svg" alt="Memoir" />
+  <header class={className}>
+    <a href="/tracklists/1">
+      <img class={logoClassName} src="/images/logo.svg" alt="Memoir" />
     </a>
   </header>
 );

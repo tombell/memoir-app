79b4b4fd35785e5219ac8479a6068cbdd9db176c
Tom Bell <tomb@tomb.io>
2021-06-21 22:30:46 +0100

    Refactor Footer.tsx to use g-style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,49 @@
 import { h } from "preact";
+import { css } from "g-style";
+
+import Breakpoints from "components/atoms/Breakpoints";
+
+const className = css({
+  display: "flex",
+  margin: "3rem 1rem 1rem",
+  fontSize: "0.75rem",
+  fontWeight: "bold",
+  [Breakpoints.Desktop]: {
+    margin: "5rem 0 3.125rem 0",
+  },
+});
+
+const leftClassName = css({
+  flex: 1,
+});
+
+const rightClassName = css({
+  display: "flex",
+  flex: 1,
+  justifyContent: "flex-end",
+});
+
+const linkClassName = css({
+  display: "block",
+  marginBottom: "0.5rem",
+  [Breakpoints.Desktop]: {
+    display: "inline",
+    marginRight: "1rem",
+  },
+});
 
 export default () => (
-  <footer class="footer">
-    <div class="footer__left">
-      <a class="footer__link" href="/tracklists/1">
+  <footer class={className}>
+    <div class={leftClassName}>
+      <a class={linkClassName} href="/tracklists/1">
         Tracklists
       </a>
-      <a class="footer__link" href="/tracks/mostplayed">
+      <a class={linkClassName} href="/tracks/mostplayed">
         Most Played Tracks
       </a>
     </div>
-    <div class="footer__right">
-      <a class="footer__link" href="https://mixcloud.com/iamdjriff">
+    <div class={rightClassName}>
+      <a class={linkClassName} href="https://mixcloud.com/iamdjriff">
         IAMDJRIFF on Mixcloud
       </a>
     </div>

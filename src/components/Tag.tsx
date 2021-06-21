a0e45d59ba3f57c092ac6a5f4cc2605d51138629
Tom Bell <tomb@tomb.io>
2021-06-21 22:55:16 +0100

    Refactor Tag.tsx to use g-style

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,11 +1,33 @@
 import { h } from "preact";
+import { css } from "g-style";
+
+import Colors from "components/atoms/Colors";
+
+const className = css({
+  display: "inline-block",
+  padding: "0.1875rem 0.3125rem",
+  marginRight: "0.5rem",
+  fontSize: "0.75rem",
+  fontWeight: "bold",
+  color: Colors.black,
+  textAlign: "center",
+  background: Colors.secondary,
+  borderRadius: "0.1875rem",
+
+  "&.purple": { background: Colors.purple },
+  "&.lilac": { background: Colors.lilac },
+  "&.blue": { background: Colors.blue },
+  "&.green": { background: Colors.green },
+  "&.light-blue": { background: Colors.lightBlue },
+});
+
+type TagColor = "purple" | "lilac" | "blue" | "green" | "light-blue";
 
 interface Props {
   text: string | number;
-  color?: string;
+  color?: TagColor;
 }
 
-export default ({ text, color }: Props) => {
-  const klass = color ? `tag--${color}` : "";
-  return <div class={`tag ${klass}`}>{text}</div>;
-};
+export default ({ text, color }: Props) => (
+  <div class={[className, color].join(" ")}>{text}</div>
+);

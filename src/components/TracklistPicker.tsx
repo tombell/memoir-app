448669a385f9625f4a244089347279358e9a6836
Tom Bell <tomb@tomb.io>
2021-06-21 23:42:58 +0100

    Refactor TracklistPicker.tsx to use g-style

--- src/components/TracklistPicker.tsx
+++ src/components/TracklistPicker.tsx
@@ -1,10 +1,33 @@
 import { h } from "preact";
 import { useCallback, useState } from "preact/hooks";
+import { css } from "g-style";
 
 import parse from "services/tracklists";
 
 import FilePicker from "components/form/FilePicker";
 import Tag from "components/Tag";
+
+const tracksClassName = css({
+  boxSizing: "border-box",
+  width: "100%",
+  padding: "1rem",
+  listStylePosition: "inside",
+  listStyleType: "decimal",
+  /* background: darken($background-color, 5%); */
+  borderRadius: "0.1875rem",
+});
+
+const trackClassName = css({
+  margin: "0.5rem 0",
+  fontSize: "0.75rem",
+  "&:last-of-type": {
+    marginBottom: 0,
+  },
+});
+
+const tagsClassName = css({
+  marginTop: "0.5rem",
+});
 
 interface Props {
   onSelect: (tracks: string[][]) => void;
@@ -29,20 +52,20 @@ export default ({ onSelect }: Props) => {
   }, []);
 
   return (
-    <div class="tracklist-picker">
+    <div>
       {!tracks && <FilePicker accept="text/plain" onSelect={handleSelect} />}
 
       {tracks && (
-        <ol class="tracklist-picker__tracks">
+        <ol class={tracksClassName}>
           {tracks.map((track) => (
-            <li class="tracklist-picker__track">
+            <li class={trackClassName}>
               <p>
                 {track[1]}
                 {" - "}
                 {track[0]}
               </p>
 
-              <div class="tracklist-picker__tags">
+              <div class={tagsClassName}>
                 <Tag text={track[2]} color="purple" />
                 <Tag text={track[3]} color="lilac" />
                 <Tag text={track[4]} color="blue" />

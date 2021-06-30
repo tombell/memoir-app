a1cea3b291c2c311d5014aee4cc32611e01847d9
Tom Bell <tomb@tomb.io>
2021-06-30 20:24:50 +0100

    Add center column layout component

---
+++ src/components/layouts/CenterColumn.tsx
@@ -0,0 +1,26 @@
+import { h } from "preact";
+import { css } from "g-style";
+
+import Colors from "components/atoms/Colors";
+
+const className = css({
+  color: Colors.primary,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const colClassName = css({
+  flex: 1,
+  maxWidth: "40rem",
+});
+
+interface Props {
+  children: any;
+}
+
+export default ({ children }: Props) => (
+  <div class={className}>
+    <div class={colClassName}>{children}</div>
+  </div>
+);

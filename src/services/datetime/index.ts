699ba906549d47dd24f068d14cecd4ac9b58b7b7
Tom Bell <tomb@tomb.io>
2021-10-19 21:34:32 +0100

    Add formatYearMonthDay function

--- src/services/datetime/index.ts
+++ src/services/datetime/index.ts
@@ -8,4 +8,14 @@ const formatDate = (date: Date): string => {
   return parts.join("/");
 };
 
+export const formatYearMonthDay = (date: Date): string => {
+  const parts = [
+    date.getFullYear(),
+    (date.getMonth() + 1).toString().padStart(2, "0"),
+    date.getDate().toString().padStart(2, "0"),
+  ];
+
+  return parts.join("-");
+}
+
 export default formatDate;

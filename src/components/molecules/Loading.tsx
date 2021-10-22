ea7dd25f40d719945e0a48fdd0789da39e5df37d
Tom Bell <tomb@tomb.io>
2021-10-23 00:28:23 +0100

    Update Loading to use Tailwind CSS

--- src/components/molecules/Loading.tsx
+++ src/components/molecules/Loading.tsx
@@ -1,41 +1,27 @@
-import { css } from "g-style";
 import { h } from "preact";
 
-import Colors from "components/atoms/Colors";
-
-const className = css({
-  display: "flex",
-  alignItems: "center",
-  color: Colors.white,
-});
-
-const spinnerClassName = css({
-  "@keyframes spin": {
-    "0%": {
-      transform: "rotate(0deg)",
-    },
-    "100%": {
-      transform: "rotate(360deg)",
-    },
-  },
-
-  width: 36,
-  height: 36,
-  padding: 0,
-  border: `0.25rem solid ${Colors.primary}`,
-  borderTop: `0.25rem solid ${Colors.secondary}`,
-  borderRight: `0.25rem solid ${Colors.secondary}`,
-  borderRadius: "50%",
-  animation: "spin 1s linear infinite",
-});
-
-const textClassName = css({
-  marginLeft: "1rem",
-});
-
 export default () => (
-  <div class={className}>
-    <div class={spinnerClassName} />
-    <h2 class={textClassName}>Loading...</h2>
+  <div class="flex items-center text-white">
+    <svg
+      class="animate-spin h-5 w-5 text-green-600"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      />
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      />
+    </svg>
+    <h2 class="ml-3">Loading...</h2>
   </div>
 );

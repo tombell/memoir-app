c1221f15bdba00f6d9de8bfef063bb711c4b6411
Tom Bell <tomb@tomb.io>
2021-10-22 23:39:41 +0100

    Update TracklistItem to use TailwindCSS

--- src/components/organisms/TracklistItem.tsx
+++ src/components/organisms/TracklistItem.tsx
@@ -1,50 +1,9 @@
-import { css } from "g-style";
-import { Fragment, h } from "preact";
-
-import Breakpoints from "components/atoms/Breakpoints";
-import Colors from "components/atoms/Colors";
+import { h } from "preact";
 
 import Link from "components/molecules/Link";
 import Tag from "components/molecules/Tag";
 
 import { formatFriendlyDate } from "services/datetime";
-
-const className = css({
-  alignItems: "center",
-  borderRadius: "0.1875rem",
-  flexWrap: "wrap",
-  margin: "0 0.5rem 1rem 0.5rem",
-  padding: "0.5rem",
-  [Breakpoints.desktop]: {
-    margin: "0 0 0.625rem 0",
-  },
-});
-
-const detailsClassName = css({
-  alignItems: "center",
-  display: "flex",
-});
-
-const imgClassName = css({
-  border: `0.0625rem solid ${Colors.greyDark}`,
-  marginRight: "0.75rem",
-});
-
-const artClassName = css({
-  height: "5rem",
-  width: "5rem",
-});
-
-const nameClassName = css({
-  flex: 1,
-  fontWeight: "bold",
-  lineHeight: 1.3,
-  margin: 0,
-});
-
-const tagsClassName = css({
-  marginLeft: 'auto',
-});
 
 export interface Props {
   id: string;
@@ -62,30 +21,29 @@ export default ({ id, name, date, artwork, trackCount }: Props) => {
 
   const background = {
     backgroundImage: `${linearGradient}, url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
   };
 
   return (
     <Link href={`/tracklist/${id}`}>
-      <div class={className} style={background}>
-        <div class={detailsClassName}>
-          <div class={imgClassName}>
+      <div
+        class="items-center rounded mb-2.5 p-2 bg-cover bg-no-repeat"
+        style={background}
+      >
+        <div class="flex items-center">
+          <div class="border border-gray-600 border-solid mr-3">
             <img
-              class={artClassName}
+              class="w-20 h-20"
               alt={`${name} Artwork`}
               src={backgroundImage}
             />
           </div>
 
-          <>
-            <h3 class={nameClassName}>{name}</h3>
+          <h3 class="flex-1 m-0 leading-5 font-bold">{name}</h3>
 
-            <div class={tagsClassName}>
-              <Tag text={`${trackCount} Tracks`} color="lilac" />
-              <Tag text={formatFriendlyDate(date)} color="light-blue" />
-            </div>
-          </>
+          <div class="ml-auto">
+            <Tag text={`${trackCount} Tracks`} color="lilac" />
+            <Tag text={formatFriendlyDate(date)} color="light-blue" />
+          </div>
         </div>
       </div>
     </Link>

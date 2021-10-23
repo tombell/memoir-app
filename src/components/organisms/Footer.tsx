8c200a3a9ccf0f3dd8e4bad4baf4ada77d115746
Tom Bell <tomb@tomb.io>
2021-10-23 15:10:12 +0100

    Use space-x-* for Footer

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -4,17 +4,17 @@ import Link from "components/molecules/Link";
 
 export default () => (
   <footer class="flex font-bold text-xs">
-    <div class="flex-1">
-      <Link className="inline mb-2 mr-4" href="/tracklists/1">
+    <div class="flex-1 space-x-4">
+      <Link className="inline" href="/tracklists/1">
         Tracklists
       </Link>
-      <Link className="inline mb-2 mr-4" href="/tracks/mostplayed">
+      <Link className="inline" href="/tracks/mostplayed">
         Most Played Tracks
       </Link>
     </div>
 
     <div class="flex flex-1 justify-end">
-      <Link className="inline mb-2 mr-4" href="https://mixcloud.com/iamdjriff">
+      <Link className="inline" href="https://mixcloud.com/iamdjriff">
         IAMDJRIFF on Mixcloud
       </Link>
     </div>

f1c7a7a2bc18597d1aa6af2f7a3335bbbbb29bee
Tom Bell <tomb@tomb.io>
2021-10-23 01:00:06 +0100

    Update CenterColumn to use Tailwind CSS

--- src/components/layouts/CenterColumn.tsx
+++ src/components/layouts/CenterColumn.tsx
@@ -1,26 +1,11 @@
-import { css } from "g-style";
 import { h } from "preact";
-
-import Colors from "components/atoms/Colors";
-
-const className = css({
-  color: Colors.primary,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-});
-
-const colClassName = css({
-  flex: 1,
-  maxWidth: "40rem",
-});
 
 interface Props {
   children: any;
 }
 
 export default ({ children }: Props) => (
-  <div class={className}>
-    <div class={colClassName}>{children}</div>
+  <div class="flex items-center justify-center text-white">
+    <div class="flex-1 max-w-2xl">{children}</div>
   </div>
 );

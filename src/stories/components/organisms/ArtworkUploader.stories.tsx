9b1dc7aeb260fb7aceda97372c8e41ec801dad96
Tom Bell <tomb@tomb.io>
2021-10-23 14:06:27 +0100

    Add fetch mocking for artwork upload

--- src/stories/components/organisms/ArtworkUploader.stories.tsx
+++ src/stories/components/organisms/ArtworkUploader.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, Story } from "@storybook/preact";
 import { h } from "preact";
+import withMock from 'storybook-addon-mock';
 
 import ArtworkUploader, { Props } from "components/organisms/ArtworkUploader";
 
@@ -7,6 +8,7 @@ const meta: Meta = {
   component: ArtworkUploader,
   title: "Components/Organisms/ArtworkUploader",
   argTypes: { onUpload: { action: "upload" } },
+  decorators: [withMock],
 };
 export default meta;
 
@@ -14,4 +16,15 @@ const Template: Story<Props> = (props) => <ArtworkUploader {...props} />;
 
 export const Base = Template.bind({});
 Base.storyName = "ArtworkUploader";
-Base.args = {};
+Base.parameters = {
+  mockData: [
+    {
+      url: "http://localhost:8080/uploads/artwork",
+      method: "POST",
+      status: 200,
+      response: {
+        key: "0cd728bbdbb38f2c1d451b3dea6dd18e.png",
+      },
+    },
+  ],
+};

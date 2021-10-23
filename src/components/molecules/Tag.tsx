5fd1bf4523d3a52014a1bf5fc15d14552878c7b0
Tom Bell <tomb@tomb.io>
2021-10-23 13:19:57 +0100

    Fix tag background colours

--- src/components/molecules/Tag.tsx
+++ src/components/molecules/Tag.tsx
@@ -3,11 +3,11 @@ import { h } from "preact";
 type TagColor = "blue" | "green" | "lightBlue" | "lilac" | "purple";
 
 const colors = {
-  blue: "blue-600",
-  green: "green-600",
-  lightBlue: "blue-400",
-  lilac: "indigo-500",
-  purple: "purple-600",
+  blue: "bg-blue-600",
+  green: "bg-green-600",
+  lightBlue: "bg-blue-400",
+  lilac: "bg-indigo-500",
+  purple: "bg-purple-600",
 };
 
 export interface Props {
@@ -17,7 +17,7 @@ export interface Props {
 
 export default ({ text, color = "green" }: Props) => (
   <div
-    class={`inline-block py-0.5 px-2 mr-2 font-bold text-xs text-center text-white rounded bg-${colors[color]}`}
+    class={`inline-block py-0.5 px-2 mr-2 font-bold text-xs text-center text-white rounded ${colors[color]}`}
   >
     {text}
   </div>

850607dd10cb5505cb79e5da4c3052abc27748b0
Tom Bell <tomb@tomb.io>
2021-10-23 18:58:46 +0100

    Remove center prop from Header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,12 +2,8 @@ import { h } from "preact";
 
 import Link from "components/molecules/Link";
 
-export interface Props {
-  center?: boolean;
-}
-
-export default ({ center = false }: Props) => (
-  <header class={`w-48 ${center ? "mx-auto my-0" : ""}`}>
+export default () => (
+  <header>
     <Link href="/tracklists/1">
       <img width={120} height={88} src="/images/logo.svg" alt="Memoir" />
     </Link>
--- src/stories/components/organisms/Header.stories.tsx
+++ src/stories/components/organisms/Header.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, Story } from "@storybook/preact";
 import { h } from "preact";
 
-import Header, { Props } from "components/organisms/Header";
+import Header from "components/organisms/Header";
 
 const meta: Meta = {
   component: Header,
@@ -9,12 +9,7 @@ const meta: Meta = {
 };
 export default meta;
 
-const Template: Story<Props> = (props) => <Header {...props} />;
+const Template: Story = () => <Header />;
 
 export const Base = Template.bind({});
 Base.storyName = "Header";
-
-export const Center = Template.bind({});
-Center.args = {
-  center: true,
-};

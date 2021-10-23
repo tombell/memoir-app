a4c39d646149d29ee84f3c2beef0afa6b66f6a99
Tom Bell <tomb@tomb.io>
2021-10-23 01:09:17 +0100

    Update Genres to use Tailwind CSS

--- src/components/organisms/Genres.tsx
+++ src/components/organisms/Genres.tsx
@@ -1,22 +1,15 @@
-import { css } from "g-style";
-import { h } from "preact";
+import { Fragment, h } from "preact";
 
 import Tag from "components/molecules/Tag";
-
-const className = css({
-  display: "flex",
-  margin: "0 0.5rem 1rem",
-  justifyContext: "space-between",
-});
 
 export interface Props {
   genres: string[];
 }
 
 export default ({ genres }: Props) => (
-  <div class={className}>
+  <>
     {genres.sort().map((genre) => (
       <Tag text={genre} color="blue" />
     ))}
-  </div>
+  </>
 );

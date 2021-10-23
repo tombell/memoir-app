59d97dbbbfe2a9180f0ead46bc9dd3befb7be19c
Tom Bell <tomb@tomb.io>
2021-10-23 19:36:24 +0100

    Simplify date formatting functions

--- src/services/datetime/index.ts
+++ src/services/datetime/index.ts
@@ -1,19 +1,13 @@
-export const formatFriendlyDate = (date: Date): string => {
-  const parts = [
+export const formatFriendlyDate = (date: Date) =>
+  [
     date.getDate().toString().padStart(2, "0"),
     (date.getMonth() + 1).toString().padStart(2, "0"),
     date.getFullYear(),
-  ];
+  ].join("/");
 
-  return parts.join("/");
-};
-
-export const formatYearMonthDay = (date: Date): string => {
-  const parts = [
+export const formatYearMonthDay = (date: Date) =>
+  [
     date.getFullYear(),
     (date.getMonth() + 1).toString().padStart(2, "0"),
     date.getDate().toString().padStart(2, "0"),
-  ];
-
-  return parts.join("-");
-};
+  ].join("-");

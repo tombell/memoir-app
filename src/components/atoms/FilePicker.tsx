8dd59af005cd520b386234f4036a943ca2fe2dc0
Tom Bell <tomb@tomb.io>
2021-12-22 19:03:13 +0000

    Add file input styles to FilePicker

--- src/components/atoms/FilePicker.tsx
+++ src/components/atoms/FilePicker.tsx
@@ -18,7 +18,13 @@ export default ({ accept, onSelect }: Props) => {
 
   return (
     <div class="p-3 text-white bg-gray-800 rounded border border-gray-700 border-solid">
-      <input type="file" ref={input} accept={accept} onChange={handleChange} />
+      <input
+        class="block w-full text-sm text-white file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-white-50 file:text-gray-900 hover:file:bg-white-100"
+        type="file"
+        ref={input}
+        accept={accept}
+        onChange={handleChange}
+      />
     </div>
   );
 };

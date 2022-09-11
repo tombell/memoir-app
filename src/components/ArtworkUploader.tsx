1af47365bbb50003e7f0d4b63f14acf60fbec539
Tom Bell <tomb@tomb.io>
2022-09-11 18:18:51 +0100

    Refactor ArtworkUploader.tsx tailwind styles into ArtworkUploader.css

--- src/components/ArtworkUploader.tsx
+++ src/components/ArtworkUploader.tsx
@@ -4,6 +4,8 @@ import FilePicker from "components/FilePicker";
 
 import { uploadArtwork } from "services/memoir";
 
+import "./ArtworkUploader.css";
+
 export interface Props {
   onUpload: (artwork: string) => void;
 }
@@ -23,22 +25,16 @@ const ArtworkUploader = ({ onUpload }: Props) => {
     [onUpload]
   );
 
-  return (
-    <>
-      {!artwork && (
-        <FilePicker accept="image/jpeg, image/png" onSelect={handleSelect} />
-      )}
-
-      {artwork && (
-        <div class="p-4 w-full text-center bg-gray-800 rounded box-border">
-          <img
-            class="w-24 h-24 border border-gray-700 border-solid"
-            alt="Mix Artwork"
-            src={`${MEMOIR_CDN_URL}/${artwork}`}
-          />
-        </div>
-      )}
-    </>
+  return artwork ? (
+    <div class="artwork-uploader">
+      <img
+        class="artwork-uploader-image"
+        alt="Mix Artwork"
+        src={`${MEMOIR_CDN_URL}/${artwork}`}
+      />
+    </div>
+  ) : (
+    <FilePicker accept="image/jpeg, image/png" onSelect={handleSelect} />
   );
 };
 

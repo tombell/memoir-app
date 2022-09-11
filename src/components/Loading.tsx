b76f8905d5d7344bfd6694bf229b518e1d673c73
Tom Bell <tomb@tomb.io>
2022-09-11 18:49:13 +0100

    Refactor Loading.tsx tailwind styles into Loading.css

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,9 @@
+import "./Loading.css";
+
 const Loading = () => (
-  <div class="flex items-center text-white">
+  <div class="loading">
     <svg
-      class="w-5 h-5 text-green-600 animate-spin"
+      class="loading-circle"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -20,7 +22,7 @@ const Loading = () => (
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       />
     </svg>
-    <h2 class="ml-3">Loading...</h2>
+    <h2 class="loading-text">Loading...</h2>
   </div>
 );
 

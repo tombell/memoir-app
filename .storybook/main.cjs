54f2f302bda6aa7d37b212b380c69291508f8c6d
Tom Bell <tomb@tomb.io>
2022-09-11 15:25:25 +0100

    Update storybook to use staticDirs after flag is deprecated

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,7 @@ const { default: tsconfigPaths } = require("vite-tsconfig-paths");
 module.exports = {
   viteFinal: (config) => mergeConfig(config, { plugins: [tsconfigPaths()] }),
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",

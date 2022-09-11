285599ec43db1565e1de68a5735c4b0ed4f8b85c
Tom Bell <tomb@tomb.io>
2022-09-11 18:20:38 +0100

    Refactor Button.tsx tailwind styles into Button.css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,12 @@
+import "./Button.css";
+
 export interface Props {
   text: string;
   onClick: () => void;
 }
 
 const Button = ({ text, onClick }: Props) => (
-  <button
-    class="py-3 px-6 font-bold text-white bg-gray-800 rounded border-gray-700 border-solid hover:bg-gray-700"
-    type="button"
-    onClick={onClick}
-  >
+  <button class="button" type="button" onClick={onClick}>
     {text}
   </button>
 );

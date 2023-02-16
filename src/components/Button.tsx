14960f521f8a9f75034922e99e500e4aab0e6937
Tom Bell <tomb@tomb.io>
2023-02-16 00:22:50 +0000

    Refactor large classes for button into the classes object

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,14 +7,14 @@ interface Props {
 }
 
 const classes = {
-  large: "",
+  large: "py-2 px-6 text-base",
   medium: "py-2 px-4 text-sm",
   small: "py-2 px-4 text-xs",
 };
 
 const Button = ({ text, variant = "large", onClick }: Props) => (
   <button
-    class={`rounded border-solid border-gray-700 bg-gray-800 py-2 px-6 text-base font-bold text-white hover:bg-gray-700 ${classes[variant]}`}
+    class={`rounded border-solid border-gray-700 bg-gray-800 font-bold text-white hover:bg-gray-700 ${classes[variant]}`}
     type="button"
     onClick={onClick}
   >

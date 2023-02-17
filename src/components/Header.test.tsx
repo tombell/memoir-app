74e302f82415e2642e6d0ae691486e317f7757ca
Tom Bell <tomb@tomb.io>
2023-02-17 17:48:30 +0000

    Add Header tests

---
+++ src/components/Header.test.tsx
@@ -0,0 +1,22 @@
+import { cleanup, render, screen } from "@testing-library/preact";
+import { afterEach, describe, expect, test } from "vitest";
+
+import Header from "components/Header";
+
+describe("Header", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("renders the image", () => {
+    render(<Header />);
+
+    const header = screen.getByRole("link");
+
+    expect(header.getAttribute("href")).toBe("/tracklists/1");
+
+    const img = screen.getByRole("img");
+
+    expect(img.getAttribute("src")).toBe("/images/logo.svg");
+  });
+});

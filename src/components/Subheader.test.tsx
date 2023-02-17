6ab83f99d6028fbb33af515dbf5bf5532001aea6
Tom Bell <tomb@tomb.io>
2023-02-17 19:12:39 +0000

    Add Subheader tests

---
+++ src/components/Subheader.test.tsx
@@ -0,0 +1,30 @@
+import { cleanup, render, screen } from "@testing-library/preact";
+import { afterEach, describe, expect, test } from "vitest";
+
+import Subheader from "components/Subheader";
+
+describe("Subheader", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  const defaultProps = {
+    text: "My subheader",
+  };
+
+  test("renders the text", () => {
+    render(<Subheader {...defaultProps} />);
+
+    const subheader = screen.getByText("My subheader");
+
+    expect(subheader).not.toBeNull();
+  });
+
+  test("renders in the center", () => {
+    render(<Subheader {...defaultProps} center />);
+
+    const subheader = screen.getByText("My subheader");
+
+    expect(subheader.classList.contains("text-center")).toBeTruthy();
+  });
+});

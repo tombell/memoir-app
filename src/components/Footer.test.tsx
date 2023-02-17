6a099996230f75f8aa04e6c4ef1088879068f3b7
Tom Bell <tomb@tomb.io>
2023-02-17 00:04:42 +0000

    Add Footer tests

---
+++ src/components/Footer.test.tsx
@@ -0,0 +1,27 @@
+import { cleanup, render, screen } from "@testing-library/preact";
+import { afterEach, describe, expect, test } from "vitest";
+
+import Footer from "components/Footer";
+
+describe("Footer", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("renders the links", () => {
+    render(<Footer />);
+
+    const links = screen.getAllByRole("link");
+
+    expect(links[0].innerHTML).toBe("Tracklists");
+    expect(links[0].getAttribute("href")).toBe("/tracklists/1");
+
+    expect(links[1].innerHTML).toBe("Most Played Tracks");
+    expect(links[1].getAttribute("href")).toBe("/tracks/mostplayed");
+
+    expect(links[2].innerHTML).toBe("IAMDJRIFF on Mixcloud");
+    expect(links[2].getAttribute("href")).toBe(
+      "https://mixcloud.com/iamdjriff"
+    );
+  });
+});

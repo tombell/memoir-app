6f253c43934042565c72cc4021202a3d69f6e4c6
Tom Bell <tomb@tomb.io>
2023-02-17 17:44:43 +0000

    Add Genres tests

---
+++ src/components/Genres.test.tsx
@@ -0,0 +1,18 @@
+import { cleanup, render, screen } from "@testing-library/preact";
+import { afterEach, describe, expect, test } from "vitest";
+
+import Genres from "components/Genres";
+
+describe("Genre", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("renders the tags", () => {
+    render(<Genres genres={["Disco", "Tech House", "Trance"]} />);
+
+    expect(screen.getByText("Disco")).toBeDefined();
+    expect(screen.getByText("Tech House")).toBeDefined();
+    expect(screen.getByText("Trance")).toBeDefined();
+  });
+});

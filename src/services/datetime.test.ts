0da491b9a0abd5f036ae249653baeaca93c94330
Tom Bell <tomb@tomb.io>
2023-02-17 19:43:20 +0000

    Update datetime tests

--- src/services/datetime.test.ts
+++ src/services/datetime.test.ts
@@ -2,32 +2,30 @@ import { describe, expect, test } from "vitest";
 
 import { formatFriendlyDate, formatYearMonthDay } from "services/datetime";
 
-describe("datetime", () => {
-  describe("formatFriendlyDate", () => {
-    test("returns the formatted string with zero padded month", () => {
-      const date = formatFriendlyDate(new Date(2020, 1, 23));
+describe("formatFriendlyDate", () => {
+  test("returns the formatted string with zero padded month", () => {
+    const date = formatFriendlyDate(new Date(2020, 1, 23));
 
-      expect(date).toBe("23/02/2020");
-    });
-
-    test("returns the formatted string with zero padded day", () => {
-      const date = formatFriendlyDate(new Date(2020, 11, 3));
-
-      expect(date).toBe("03/12/2020");
-    });
+    expect(date).toBe("23/02/2020");
   });
 
-  describe("formatYearMonthDay", () => {
-    test("returns the formatted string with zero padded month", () => {
-      const date = formatYearMonthDay(new Date(2020, 1, 23));
+  test("returns the formatted string with zero padded day", () => {
+    const date = formatFriendlyDate(new Date(2020, 11, 3));
 
-      expect(date).toBe("2020-02-23");
-    });
+    expect(date).toBe("03/12/2020");
+  });
+});
 
-    test("returns the formatted string with zero padded day", () => {
-      const date = formatYearMonthDay(new Date(2020, 11, 3));
+describe("formatYearMonthDay", () => {
+  test("returns the formatted string with zero padded month", () => {
+    const date = formatYearMonthDay(new Date(2020, 1, 23));
 
-      expect(date).toBe("2020-12-03");
-    });
+    expect(date).toBe("2020-02-23");
+  });
+
+  test("returns the formatted string with zero padded day", () => {
+    const date = formatYearMonthDay(new Date(2020, 11, 3));
+
+    expect(date).toBe("2020-12-03");
   });
 });

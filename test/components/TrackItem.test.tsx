595d44cf79ab0976c2ab6a1c0da6d114076514f5
Tom Bell <tomb@tomb.io>
2023-02-20 20:01:17 +0000

    Add TrackItem tests

---
+++ test/components/TrackItem.test.tsx
@@ -0,0 +1,30 @@
+import { cleanup, render, screen } from "@testing-library/preact";
+import { afterEach, describe, expect, test } from "vitest";
+
+import TrackItem from "components/TrackItem";
+
+describe("TrackItem", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("renders the links", () => {
+    render(
+      <TrackItem
+        id="asdf-asdf-asdf"
+        artist="Artist"
+        name="Track Name"
+        genre="Tech House"
+        bpm={123.0}
+        camelotKey="12B"
+      />
+    );
+
+    const link = screen.getByRole("link", { name: "Artist - Track Name" });
+
+    expect(link.getAttribute("href")).toBe("/tracks/asdf-asdf-asdf");
+    expect(screen.getByText("123.00")).toBeDefined();
+    expect(screen.getByText("12B")).toBeDefined();
+    expect(screen.getByText("Tech House")).toBeDefined();
+  });
+});

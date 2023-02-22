d4c7d759808b3208a4e8bd0680188eccb7c43fe7
Tom Bell <tomb@tomb.io>
2023-02-22 23:41:05 +0000

    Add wrapped memoir API hooks

---
+++ src/hooks/memoir.ts
@@ -0,0 +1,15 @@
+import useGetResource from "hooks/useGetResource";
+
+import { Track, Tracklist } from "services/memoir/types";
+
+export const useTracklists = (page: number) =>
+  useGetResource<Tracklist[]>("/tracklists", page);
+
+export const useTracklistsByTrack = (id: string, page: number) =>
+  useGetResource<Tracklist[]>(`/tracks/${id!}/tracklists`, page);
+
+export const useTracklist = (id: string) =>
+  useGetResource<Tracklist>(`/tracklists/${id}`);
+
+export const useMostPlayedTracks = () =>
+  useGetResource<Track[]>("/tracks/mostplayed");

aab07dbab0a213275c069309bd64e0f5a93240e4
Tom Bell <tomb@tomb.io>
2023-02-23 02:33:12 +0000

    Refactor Pagination tests

--- test/components/Pagination.test.tsx
+++ test/components/Pagination.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render, screen } from "@testing-library/preact";
+import { cleanup, render } from "@testing-library/preact";
 import { afterEach, describe, expect, test } from "vitest";
 
 import Pagination from "components/Pagination";
@@ -13,63 +13,71 @@ describe("Pagination", () => {
   };
 
   test("renders nothing when only one page", () => {
-    render(<Pagination {...defaultProps} page={1} hasMore={false} />);
+    const { queryByText } = render(
+      <Pagination {...defaultProps} page={1} hasMore={false} />
+    );
 
-    const prev = screen.queryByText("← Newer");
+    const prev = queryByText("← Newer");
 
     expect(prev).toBeNull();
 
-    const next = screen.queryByText("Older →");
+    const next = queryByText("Older →");
 
     expect(next).toBeNull();
   });
 
   test("renders next link when has more", () => {
-    render(<Pagination {...defaultProps} page={1} hasMore />);
+    const { queryByText, getByText } = render(
+      <Pagination {...defaultProps} page={1} hasMore />
+    );
 
-    const prev = screen.queryByText("← Newer");
+    const prev = queryByText("← Newer");
 
     expect(prev).toBeNull();
 
-    const next = screen.getByText("Older →");
+    const next = getByText("Older →");
 
     expect(next.getAttribute("href")).toBe("/tracklists?page=2");
   });
 
   test("renders previous link and not next link when not on page one and does not have more", () => {
-    render(<Pagination {...defaultProps} page={2} hasMore={false} />);
+    const { queryByText, getByText } = render(
+      <Pagination {...defaultProps} page={2} hasMore={false} />
+    );
 
-    const prev = screen.getByText("← Newer");
+    const prev = getByText("← Newer");
 
     expect(prev.getAttribute("href")).toBe("/tracklists?page=1");
 
-    const next = screen.queryByText("Older →");
+    const next = queryByText("Older →");
 
     expect(next).toBeNull();
   });
 
   test("renders previous link and next link when not on page one and does have more", () => {
-    render(<Pagination {...defaultProps} page={2} hasMore />);
+    const { getByText } = render(
+      <Pagination {...defaultProps} page={2} hasMore />
+    );
 
-    const prev = screen.getByText("← Newer");
+    const prev = getByText("← Newer");
 
     expect(prev.getAttribute("href")).toBe("/tracklists?page=1");
 
-    const next = screen.getByText("Older →");
+    const next = getByText("Older →");
 
     expect(next.getAttribute("href")).toBe("/tracklists?page=3");
   });
 
   test("renders id in the url in previous link and next link", () => {
-    render(
+    const { getByText } = render(
       <Pagination path="/tracklists/:id" id="my-resource-id" page={2} hasMore />
     );
 
-    const prev = screen.getByText("← Newer");
+    const prev = getByText("← Newer");
 
     expect(prev.getAttribute("href")).toBe("/tracklists/my-resource-id?page=1");
 
-    const next = screen.getByText("Older →");
+    const next = getByText("Older →");
 
     expect(next.getAttribute("href")).toBe("/tracklists/my-resource-id?page=3");
   });

7989d0a6e167e88aaa100097cd6d9ba59df23475
Tom Bell <tomb@tomb.io>
2023-03-19 17:53:35 +0000

    Update contrast on TrackSearch/Result highlight colour

--- src/components/TrackSearch/Result.tsx
+++ src/components/TrackSearch/Result.tsx
@@ -9,7 +9,7 @@ const highlight = (text?: string | null) => {
 
   return text.replace(
     /<<(.*?)>>/g,
-    (_a, str) => `<b class="italic font-bold text-indigo-500">${str}</b>`
+    (_a, str) => `<b class="italic font-bold text-purple-400">${str}</b>`
   );
 };
 

7c59b733edd1c53390df4ae421017374d45f3227
Tom Bell <tomb@tomb.io>
2023-03-19 18:05:34 +0000

    Update FilePicker with name and label props

--- src/components/FilePicker.stories.tsx
+++ src/components/FilePicker.stories.tsx
@@ -14,6 +14,15 @@ export const Base = {
   name: "FilePicker",
 
   args: {
+    name: "filepicker",
+    accept: "text/plain",
+  },
+};
+
+export const Label = {
+  args: {
+    name: "filepicker with label",
+    label: "Filepicker with label",
     accept: "text/plain",
   },
 };
--- src/components/FilePicker.tsx
+++ src/components/FilePicker.tsx
@@ -1,11 +1,13 @@
 import { useCallback, useRef } from "preact/hooks";
 
 interface Props {
+  name: string;
+  label?: string;
   accept: string;
   onSelect: (file: File) => void;
 }
 
-const FilePicker = ({ accept, onSelect }: Props) => {
+const FilePicker = ({ name, label, accept, onSelect }: Props) => {
   const input = useRef<HTMLInputElement>(null);
 
   const handleChange = useCallback(() => {
@@ -16,16 +18,21 @@ const FilePicker = ({ accept, onSelect }: Props) => {
   }, [input, onSelect]);
 
   return (
-    <div class="rounded border border-solid border-gray-700 bg-gray-800 p-3 text-white">
-      <input
-        class="block w-full text-sm text-white file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:py-2 file:px-4 file:text-sm file:font-semibold file:text-gray-900 hover:file:bg-violet-100"
-        type="file"
-        ref={input}
-        accept={accept}
-        onChange={handleChange}
-        data-testid="filepicker"
-      />
-    </div>
+    <label htmlFor={name}>
+      {label && <span class="mb-2 block font-bold text-white">{label}</span>}
+
+      <div class="rounded border border-solid border-gray-700 bg-gray-800 p-3 text-white">
+        <input
+          class="block w-full text-sm text-white file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:py-2 file:px-4 file:text-sm file:font-semibold file:text-gray-900 hover:file:bg-violet-100"
+          name={name}
+          type="file"
+          ref={input}
+          accept={accept}
+          onChange={handleChange}
+          data-testid="filepicker"
+        />
+      </div>
+    </label>
   );
 };
 

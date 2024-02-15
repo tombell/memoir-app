0e0dcdfa3b56b24cc6862974092d6f20310f3af1
Tom Bell <tomb@tomb.io>
2024-02-15 00:32:01 +0000

    Switch to happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(() => ({
   plugins: [tsconfigPaths(), preact()],
   test: {
-    environment: "jsdom",
+    environment: "happy-dom",
   },
 }));

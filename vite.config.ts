3cea0ddf10c176f292356eb6e13a6aad6b06d07b
Tom Bell <tomb@tomb.io>
2024-02-26 00:00:18 +0000

    Update vitest coverage excludes

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ export default defineConfig(() => ({
   plugins: [tsconfigPaths(), preact()],
   test: {
     environment: "happy-dom",
+    coverage: {
+      exclude: ["**/*.cjs", "**/*.d.ts", ".storybook", "test/support/*.ts"],
+    },
   },
 }));

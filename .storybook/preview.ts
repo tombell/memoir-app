ed062e3ac2926fea84fa3a6df949d02afa7ffefc
Tom Bell <tomb@tomb.io>
2024-02-29 23:44:27 +0000

    Convert storybook preview.cjs to preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,10 @@
+import { Decorator, Parameters } from "@storybook/preact";
+
 import "../src/components/App.css";
 
-export const parameters = {};
+export const parameters: Parameters = {};
 
-export const decorators = [
+export const decorators: Decorator[] = [
   (story) => {
     document.body.classList.add("font-sans", "bg-gray-900");
     return story();

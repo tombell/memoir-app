d0f68878db79b77d2dd727aecd4068789d977f74
Tom Bell <tomb@tombell.dev>
2025-01-31 00:10:51 +0000

    Update mock fetch module

--- src/test/support/fetch.ts
+++ src/test/support/fetch.ts
@@ -1,12 +1,7 @@
-import { afterEach, mock } from "bun:test";
+import { mock } from "bun:test";
 
-const mockFetch = mock();
-
+export const mockFetch = mock();
 global.fetch = mockFetch;
-
-afterEach(() => {
-  mockFetch.mockReset();
-});
 
 export const mockFetchResponse = (
   data: object,
@@ -17,11 +12,5 @@ export const mockFetchResponse = (
     headers: {
       get: (key: string) => headers[key] || null,
     },
-  });
-};
-
-export const mockFetchThrows = () => {
-  mockFetch.mockImplementation(() => {
-    throw new Error("boom town");
   });
 };

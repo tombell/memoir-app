e8b1dc9100db433d2a34a1cb7fc7dee33d58a2aa
Tom Bell <tomb@tombell.dev>
2025-01-31 21:28:45 +0000

    Add eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import js from "@eslint/js";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["src/**/*.d.ts"],
+  },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+
+    settings: {
+      react: {
+        pragma: "h",
+        version: "18",
+      },
+    },
+  },
+
+  js.configs.recommended,
+
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+
+  prettierRecommended,
+
+  {
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        { checksVoidReturn: { attributes: false } },
+      ],
+      "react/no-unknown-property": [
+        "error",
+        {
+          ignore: ["class"],
+        },
+      ],
+    },
+  },
+);

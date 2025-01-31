00f26d6707a3aa389e56316525b5854d0ecdeb16
Tom Bell <tomb@tombell.dev>
2025-01-31 00:16:54 +0000

    Update memoir service module, to export get/patch/post/postFile

--- src/test/services/memoir.test.ts
+++ src/test/services/memoir.test.ts
@@ -1,14 +1,21 @@
 import { afterEach, beforeEach, describe, expect, test } from "bun:test";
 
-import { request } from "~/services/memoir";
-
-import { mockFetchResponse } from "~/test/support/fetch";
+import {
+  type APIResponse,
+  get,
+  patch,
+  post,
+  postFile,
+} from "~/services/memoir";
+import { mockFetch, mockFetchResponse } from "~/test/support/fetch";
 
 afterEach(() => {
+  mockFetch.mockReset();
+
   import.meta.env.VITE_MEMOIR_API_KEY = "";
 });
 
-describe("request", () => {
+describe("get", () => {
   const response = { data: true };
 
   beforeEach(() => {
@@ -17,28 +24,55 @@ describe("request", () => {
 
   describe("without an api key", () => {
     test("returns data with a get request", async () => {
-      const resp = await request("/endpoint");
-      const data = await resp.json();
+      const data = await get("/endpoint");
 
       expect(data).toStrictEqual(response);
 
-      expect(fetch).toHaveBeenCalledWith("/api/endpoint", {
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
         method: "GET",
         body: null,
         headers: {},
       });
     });
+  });
 
-    test("returns data with a post request with a body", async () => {
-      const body = JSON.stringify({ foo: "bar" });
-      const resp = await request("/endpoint", "POST", body);
-      const data = await resp.json();
+  describe("with an api key", () => {
+    beforeEach(() => {
+      import.meta.env.VITE_MEMOIR_API_KEY = "asdf-asdf";
+    });
+
+    test("returns data with a get request", async () => {
+      const data = await get("/endpoint");
 
       expect(data).toStrictEqual(response);
 
-      expect(fetch).toHaveBeenCalledWith("/api/endpoint", {
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
+        method: "GET",
+        body: null,
+        headers: { "API-Token": "asdf-asdf" },
+      });
+    });
+  });
+});
+
+describe("patch", () => {
+  const response: APIResponse<{ value: boolean }> = { data: { value: true } };
+
+  beforeEach(() => {
+    mockFetchResponse(response);
+  });
+
+  describe("without an api key", () => {
+    test("returns data with a get request", async () => {
+      const payload = { value: false };
+
+      const data = await post("/endpoint", payload);
+
+      expect(data).toStrictEqual(response);
+
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
         method: "POST",
-        body,
+        body: JSON.stringify(payload),
         headers: { "Content-Type": "application/json" },
       });
     });
@@ -50,32 +84,120 @@ describe("request", () => {
     });
 
     test("returns data with a get request", async () => {
-      const resp = await request("/endpoint");
-      const data = await resp.json();
+      const payload = { value: false };
+
+      const data = await post("/endpoint", payload);
 
       expect(data).toStrictEqual(response);
 
-      expect(fetch).toHaveBeenCalledWith("/api/endpoint", {
-        method: "GET",
-        body: null,
-        headers: { "API-Token": "asdf-asdf" },
-      });
-    });
-
-    test("returns data with a post request with a body", async () => {
-      const body = JSON.stringify({ foo: "bar" });
-      const resp = await request("/endpoint", "POST", body);
-      const data = await resp.json();
-
-      expect(data).toStrictEqual(response);
-
-      expect(fetch).toHaveBeenCalledWith("/api/endpoint", {
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
         method: "POST",
-        body,
+        body: JSON.stringify(payload),
         headers: {
           "API-Token": "asdf-asdf",
           "Content-Type": "application/json",
         },
+      });
+    });
+  });
+});
+
+describe("post", () => {
+  const response: APIResponse<{ value: boolean }> = { data: { value: true } };
+
+  beforeEach(() => {
+    mockFetchResponse(response);
+  });
+
+  describe("without an api key", () => {
+    test("returns data with a get request", async () => {
+      const payload = { value: false };
+
+      const data = await patch("/endpoint", payload);
+
+      expect(data).toStrictEqual(response);
+
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
+        method: "PATCH",
+        body: JSON.stringify(payload),
+        headers: { "Content-Type": "application/json" },
+      });
+    });
+  });
+
+  describe("with an api key", () => {
+    beforeEach(() => {
+      import.meta.env.VITE_MEMOIR_API_KEY = "asdf-asdf";
+    });
+
+    test("returns data with a get request", async () => {
+      const payload = { value: false };
+
+      const data = await patch("/endpoint", payload);
+
+      expect(data).toStrictEqual(response);
+
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
+        method: "PATCH",
+        body: JSON.stringify(payload),
+        headers: {
+          "API-Token": "asdf-asdf",
+          "Content-Type": "application/json",
+        },
+      });
+    });
+  });
+});
+
+describe("postFile", () => {
+  const response: APIResponse<{ value: boolean }> = { data: { value: true } };
+
+  beforeEach(() => {
+    mockFetchResponse(response);
+  });
+
+  describe("without an api key", () => {
+    test("returns data with a get request", async () => {
+      const file = new File(["fake file"], "artwork.jpg", {
+        type: "image/jpeg",
+      });
+
+      const payload = new FormData();
+      payload.append("artwork", file);
+
+      const data = await postFile("/endpoint", payload);
+
+      expect(data).toStrictEqual(response);
+
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
+        method: "POST",
+        body: payload,
+        headers: {},
+      });
+    });
+  });
+
+  describe("with an api key", () => {
+    beforeEach(() => {
+      import.meta.env.VITE_MEMOIR_API_KEY = "asdf-asdf";
+    });
+
+    test("returns data with a get request", async () => {
+      const file = new File(["fake file"], "artwork.jpg", {
+        type: "image/jpeg",
+      });
+
+      const payload = new FormData();
+      payload.append("artwork", file);
+
+      const data = await postFile("/endpoint", payload);
+
+      expect(data).toStrictEqual(response);
+
+      expect(mockFetch).toHaveBeenCalledWith("/api/endpoint", {
+        method: "POST",
+        body: payload,
+        headers: { "API-Token": "asdf-asdf" },
       });
     });
   });

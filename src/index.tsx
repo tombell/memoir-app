1ece8f5433e3bf6fe168c099513ba9ac6d686fa3
Tom Bell <tomb@tombell.dev>
2025-01-31 21:31:17 +0000

    Disable @typescript-eslint/non-nullable-type-assertion-style in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,5 @@ import App from "~/App";
 
 import "./index.css";
 
+// eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 render(<App />, document.getElementById("app") as HTMLElement);

80714351ad7cc350e7c1053ba61e3e66788ccd6c
Tom Bell <tomb@tombell.dev>
2025-01-31 00:16:54 +0000

    Update memoir service module, to export get/patch/post/postFile

--- src/services/memoir/index.ts
+++ src/services/memoir/index.ts
@@ -1,4 +1,4 @@
-export const request = async (
+const request = async (
   endpoint: string,
   method = "GET",
   body: FormData | string | null = null,
@@ -19,3 +19,28 @@ export const request = async (
     headers,
   });
 };
+
+export interface APIResponse<T> {
+  meta?: {
+    current_page: number;
+    total_pages: number;
+  };
+  data: T;
+}
+
+export const get = <T>(endpoint: string): Promise<APIResponse<T>> =>
+  request(endpoint).then((r) => r.json());
+
+export const post = <T>(endpoint: string, body?: T): Promise<APIResponse<T>> =>
+  request(endpoint, "POST", body ? JSON.stringify(body) : undefined).then((r) =>
+    r.json(),
+  );
+
+export const postFile = <T>(
+  endpoint: string,
+  body: FormData,
+): Promise<APIResponse<T>> =>
+  request(endpoint, "POST", body).then((r) => r.json());
+
+export const patch = <T>(endpoint: string, body: T): Promise<APIResponse<T>> =>
+  request(endpoint, "PATCH", JSON.stringify(body)).then((r) => r.json());

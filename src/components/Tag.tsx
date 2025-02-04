1c3cd92cf1804e6f246f295bff0dfe0bc30b9de7
Tom Bell <tomb@tombell.dev>
2025-02-04 00:49:54 +0000

    Refactor Tag to use clsx

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,22 +1,25 @@
+import clsx from "clsx";
+
 export type Color = "blue" | "green" | "lightblue" | "lilac" | "purple";
 
-const classes = {
-  blue: "bg-blue-600",
-  green: "bg-lime-700",
-  lightblue: "bg-sky-700",
-  lilac: "bg-indigo-700",
-  purple: "bg-purple-600",
-};
-
 interface Props {
-  text: string | number;
   color?: Color;
+  text: string | number;
 }
 
-export default function Tag({ text, color = "green" }: Props) {
+export default function Tag({ color = "green", text }: Props) {
   return (
     <div
-      class={`inline-block rounded-sm px-2 py-1 text-center text-xs font-bold text-white ${classes[color]}`}
+      class={clsx(
+        `inline-block rounded-sm px-2 py-1 text-center text-xs font-bold text-white`,
+        {
+          "bg-blue-600": color === "blue",
+          "bg-lime-700": color === "green",
+          "bg-sky-700": color === "lightblue",
+          "bg-indigo-700": color === "lilac",
+          "bg-purple-600": color === "purple",
+        },
+      )}
     >
       {text}
     </div>

837bf3da7217192a2d7ceba916e4806b240abef6
Tom Bell <tomb@tombell.dev>
2025-02-04 19:31:29 +0000

    Add debounce util function

---
+++ src/utils/debounce.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function debounce<T extends (...args: any[]) => void>(
+  fn: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+}

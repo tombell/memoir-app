fada37a394ea7baee00c87d554e56d624fe7f72f
Tom Bell <tomb@tombell.dev>
2025-02-04 19:33:53 +0000

    Refactor track list search component

--- src/components/TrackSearch/index.tsx
+++ src/components/TrackSearch/index.tsx
@@ -1,6 +1,6 @@
 import { useStore } from "@nanostores/preact";
-import { effect, useSignal } from "@preact/signals";
-import { useCallback, useRef, useState } from "preact/hooks";
+import { map } from "nanostores";
+import { useCallback, useEffect, useRef } from "preact/hooks";
 
 import Input from "~/components/Input";
 import Results from "~/components/TrackSearch/Results";
@@ -10,55 +10,54 @@ import type { Track } from "~/services/memoir/types";
 
 import { createSearchTracksStore } from "~/stores/tracks";
 
-export default function TrackSearch() {
-  const showResults = useSignal(false);
-  const tracks = useSignal<Track[] | null>(null);
+import debounce from "~/utils/debounce";
 
+const $searchTracks = createSearchTracksStore();
+
+const $results = map<{ show: boolean; tracks: Track[] | undefined }>({
+  show: false,
+  tracks: undefined,
+});
+
+export default function TrackSearch() {
   const ref = useRef<HTMLDivElement>(null);
 
-  const [$searchTracks] = useState(createSearchTracksStore());
-  const { mutate } = useStore($searchTracks);
+  const results = useStore($results);
 
-  const onBodyClick = useCallback(
-    ({ target }: MouseEvent) => {
-      if (!ref.current?.contains(target as Node)) {
-        showResults.value = false;
-      }
-    },
-    [showResults],
-  );
-
-  const onInput = useCallback(
-    async (value: string) => {
-      if (value.length < 3) {
-        tracks.value = null;
-        showResults.value = false;
-        return;
-      }
-
-      const resp = (await mutate(value)) as APIResponse<Track[]>;
-
-      tracks.value = resp.data;
-      showResults.value = true;
-    },
-    [showResults, tracks, mutate],
-  );
-
-  const onInputFocus = useCallback(() => {
-    showResults.value = true;
-  }, [showResults]);
-
-  const onResultClick = useCallback(() => {
-    showResults.value = false;
-  }, [showResults]);
-
-  effect(() => {
-    if (showResults.value) {
-      document.addEventListener("click", onBodyClick);
-    } else {
-      document.removeEventListener("click", onBodyClick);
+  const onBodyClick = useCallback(({ target }: MouseEvent) => {
+    if (!ref.current?.contains(target as Node)) {
+      $results.setKey("show", false);
     }
-  });
+  }, []);
+
+  useEffect(() => {
+    const cleanup = $results.subscribe(({ show }) => {
+      if (show) {
+        document.addEventListener("click", onBodyClick);
+      } else {
+        document.removeEventListener("click", onBodyClick);
+      }
+    });
+
+    return cleanup;
+  }, [onBodyClick]);
+
+  const request = useCallback(
+    debounce(async (value: string) => {
+      const resp = (await $searchTracks.mutate(value)) as APIResponse<Track[]>;
+      $results.set({ show: true, tracks: resp.data });
+    }, 300),
+    [],
+  );
+
+  const onInput = useCallback((value: string) => {
+    if (value.length < 3) {
+      $results.set({ show: false, tracks: undefined });
+      return;
+    }
+
+    request(value);
+  }, []);
 
   return (
     <div class="relative" ref={ref}>
@@ -66,13 +65,13 @@ export default function TrackSearch() {
         name="search"
         placeholder="Search tracks..."
         onInput={onInput}
-        onFocus={onInputFocus}
+        onFocus={() => $results.setKey("show", true)}
       />
 
       <Results
-        tracks={tracks.value}
-        show={showResults.value}
-        onResultClick={onResultClick}
+        tracks={results.tracks}
+        show={results.show}
+        onResultClick={() => $results.setKey("show", false)}
       />
     </div>
   );

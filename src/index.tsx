29129d4bb907885991a453c1917d7da33ebd781b
Tom Bell <tomb@tombell.dev>
2026-01-16 00:39:44 +0000

    Remove import css from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import { render } from "preact";
 
 import App from "~/App";
 
-import "./index.css";
-
 // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 render(<App />, document.getElementById("app") as HTMLElement);

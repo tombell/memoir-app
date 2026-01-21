96935c110b1c04e38d8194f1fe45096fb51ca905
Tom Bell <tomb@tombell.dev>
2026-01-21 14:40:54 +0000

    Add support for HMR in bun

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,9 @@ import { render } from "preact";
 
 import App from "~/App";
 
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
+
 // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
 render(<App />, document.getElementById("app") as HTMLElement);

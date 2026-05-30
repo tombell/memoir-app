6706af6594f704f73741c8427687ca2032929425
Tom Bell <tomb@tombell.dev>
2026-05-30 11:55:34 +0100

    wip: oxfmt

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -2,42 +2,5 @@ import { defineConfig } from "oxfmt";
 
 export default defineConfig({
   ignorePatterns: [],
-  sortImports: {
-    groups: [
-      "builtin",
-      ["external", "unknown"],
-      ["internal", "parent", "sibling", "index"],
-      "style",
-      "side_effect",
-      "side_effect_style",
-    ],
-    newlinesBetween: true,
-    order: "asc",
-  },
-  // sortImports: {
-  //   customGroups: [
-  //     { groupName: "layouts", elementNamePattern: ["~/layouts/**"] },
-  //     { groupName: "pages", elementNamePattern: ["~/pages/**"] },
-  //     { groupName: "components", elementNamePattern: ["~/components/**"] },
-  //     { groupName: "services", elementNamePattern: ["~/services/**"] },
-  //     { groupName: "utils", elementNamePattern: ["~/utils/**"] },
-  //     { groupName: "tests", elementNamePattern: ["~/tests/**"] },
-  //   ],
-  //   newlinesBetween: true,
-  //   groups: [
-  //     "type-import",
-  //     ["value-builtin", "value-external"],
-  //     "layouts",
-  //     "pages",
-  //     "components",
-  //     "services",
-  //     "utils",
-  //     "tests",
-  //     "type-internal",
-  //     "value-internal",
-  //     ["type-parent", "type-sibling", "type-index"],
-  //     ["value-parent", "value-sibling", "value-index"],
-  //     "unknown",
-  //   ],
-  // },
+  sortImports: true,
 });
